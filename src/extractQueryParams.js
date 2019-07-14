51a6412ff8aeb76cdfced4ae0f4f4f85bbcd73f3
Baldur Helgason <baldur.helgason@gmail.com>
2019-07-14 08:42:49 +0000

    fix: optimise extractQueryParams

--- src/extractQueryParams.js
+++ src/extractQueryParams.js
@@ -1,15 +1,16 @@
 export default function extractQueryParams(src) {
-  const [url, query] = src.split("?");
-  const params = query
-    ? query
-        .split("&")
-        .map(function splitDecode(x) {
-          const [key, val] = x.split("=");
-          return [key, decodeURIComponent(val)];
-        })
-        .reduce(function reduceToObject(obj, [key, val]) {
-          return Object.assign(obj, { [key]: val });
-        }, {})
-    : {};
+  const splitSrc = src.split("?");
+  const url = splitSrc[0];
+  const query = splitSrc[1] || "";
+  const paramsPairs = query.split("&");
+  const params = {};
+  const len = paramsPairs.length;
+  for (let i = 0; i < len; i++) {
+    const param = paramsPairs[i];
+    const splitParam = param.split("=");
+    const key = splitParam[0];
+    const val = splitParam[1];
+    params[key] = decodeURIComponent(val);
+  }
   return [url, params];
 }
